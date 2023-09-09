294287628a8128449550c717b9dc58d6199cc5e2
8loser <8loser@gmail.com>
2023-09-09 23:40:43 +0800

    test_sampleName

--- tests/test_sampleName.py
+++ tests/test_sampleName.py
@@ -1,7 +1,15 @@
+import pytest
+import random
 from sampleName import SampleName
 
 
-def test_listName():
-    '''
-    單元測試
-    '''
+def test_listMaleName():
+    sampleNameInst = SampleName()
+    length = random.randint(1, 10)
+    assert len(sampleNameInst.listMaleName(length)) == length
+
+
+def test_listMaleName_failed():
+    sampleNameInst = SampleName()
+    with pytest.raises(TypeError):
+        sampleNameInst.listMaleName('test')
